359e1f58aae3380839627c67af44a776a13e8df0
Fellipe Augusto <Fellipe246@hotmail.com>
2024-11-14 19:10:38 -0300

    Simplificando pokeapi

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonHtml(pokemon) {
     return `    
         <li class="pokemon">
@@ -22,14 +18,9 @@ function convertPokemonHtml(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonHtml(pokemon));
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonHtml).join('')
 
-    }
-    console.log(listItems);
     
 })
 
--- src/scripts/pokeapi.js
+++ src/scripts/pokeapi.js
@@ -2,7 +2,7 @@ const pokeApi={};
 
 pokeApi.getPokemons = function(offset =0,limit=10){
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return rfetch(url)
+    return fetch(url)
     .then((response) => response.json())
     .then((jasonBody) => jasonBody.results)
     .catch((error) => console.log(error))
