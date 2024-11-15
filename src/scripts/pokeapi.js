b3fdbc2cbf30d03cc31fd750ae75799354265c64
Fellipe Augusto <Fellipe246@hotmail.com>
2024-11-14 21:24:46 -0300

    implementações

--- src/scripts/pokeapi.js
+++ src/scripts/pokeapi.js
@@ -1,9 +1,33 @@
 const pokeApi={};
 
-pokeApi.getPokemons = function(offset =0,limit=10){
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return fetch(url)
+function convertPokeApiDetail(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+    
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
     .then((response) => response.json())
-    .then((jasonBody) => jasonBody.results)
-    .catch((error) => console.log(error))
+    .then(convertPokeApiDetail)
+
+}
+
+pokeApi.getPokemons = (offset =0,limit=20) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jasonBody) => jasonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
